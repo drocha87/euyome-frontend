e9abb83da143a1925c272b71b41b78c4dd07ca71
drocha87 <dlmrocha87@gmail.com>
2020-09-24 01:01:46 -0300

    certificate

--- api/redirect-ssl.js
+++ api/redirect-ssl.js
@@ -2,12 +2,12 @@
  * https://community.umbler.com/br/t/redirecionamento-https-em-aplicacoes-nodejs/676
  */
 export default (req, res, next) => {
-  // if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
-  //   res.writeHead(301, {
-  //     Location: 'https://www.euyo.me' + req.url,
-  //   });
-  //   res.end();
-  // } else {
-  next();
-  // }
+  if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
+    res.writeHead(301, {
+      Location: 'https://www.euyo.me' + req.url,
+    });
+    res.end();
+  } else {
+    next();
+  }
 };
