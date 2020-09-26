7af008698b93a3822bdc78c67714182da455ba57
drocha87 <dlmrocha87@gmail.com>
2020-09-26 06:53:05 -0300

    ssl

--- api/redirect-ssl.js
+++ api/redirect-ssl.js
@@ -1,17 +1,16 @@
 /* code as recommend in umbler help page
  * https://community.umbler.com/br/t/redirecionamento-https-em-aplicacoes-nodejs/676
  */
-export default (req, _res, next) => {
+export default (req, res, next) => {
   if (req.originalUrl !== '/_loading/sse') {
     console.log(req.headers['user-agent'], req.originalUrl);
   }
-  next();
-  // if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
-  //   res.writeHead(301, {
-  //     Location: 'https://www.euyo.me' + req.url,
-  //   });
-  //   res.end();
-  // } else {
-  //   next();
-  // }
+  if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
+    res.writeHead(301, {
+      Location: 'https://www.euyo.me' + req.url,
+    });
+    res.end();
+  } else {
+    next();
+  }
 };
