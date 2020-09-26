d9f36d49982976ee50c1c6f036b510f3f5dc0b89
drocha87 <dlmrocha87@gmail.com>
2020-09-26 15:41:03 -0300

    status zero again (problem and more problems)

--- api/redirect-ssl.js
+++ api/redirect-ssl.js
@@ -1,7 +1,7 @@
 /* code as recommend in umbler help page
  * https://community.umbler.com/br/t/redirecionamento-https-em-aplicacoes-nodejs/676
  */
-export default (req, res, next) => {
+export default (req, _res, next) => {
   if (req.originalUrl !== '/_loading/sse') {
     console.log(
       req.originalUrl,
@@ -12,13 +12,13 @@ export default (req, res, next) => {
     );
   }
 
-  if (req.headers.host && !req.headers.host.startsWith('www.')) {
-    res.writeHead(301, `https://www.euyo.me${req.url}`);
-  } else {
-    next();
-  }
+  // if (req.headers.host && !req.headers.host.startsWith('www.')) {
+  //   res.writeHead(301, `https://www.euyo.me${req.url}`);
+  // } else {
+  //   next();
+  // }
 
-  // next();
+  next();
   // if ((req.headers['x-forwarded-proto'] || '').endsWith('http')) {
   //   res.writeHead(301, {
   //     Location: 'https://www.euyo.me' + req.url,
