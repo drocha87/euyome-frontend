0806c51241989ecc1c622aa5c8dd68b19541ad63
Diego Rocha <dlmrocha87@gmail.com>
2020-12-04 15:10:27 -0300

    Implements stats view

---
+++ components/DailyViewsChart.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <canvas id="chart" width="100%" height="250px"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable no-unused-vars */
+import Vue from 'vue';
+import { Chart } from 'chart.js';
+
+export default Vue.extend({
+  props: {
+    data: { type: Object, required: true },
+  },
+
+  mounted() {
+    const canvas = document.getElementById('chart') as HTMLCanvasElement;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chart = new Chart(canvas, {
+      type: 'line',
+      data: this.data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          callbacks: {
+            label(tooltipItem: any) {
+              return tooltipItem.yLabel;
+            },
+          },
+        },
+        scales: {
+          yAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                beginAtZero: true,
+                stepSize: 10,
+              },
+            },
+          ],
+        },
+      },
+    });
+  },
+});
+</script>
---
+++ components/LinksStats.vue
@@ -0,0 +1,98 @@
+<template>
+  <client-only>
+    <v-card width="100%">
+      <v-card-title> Análise de cliques </v-card-title>
+      <v-card-subtitle class="text-caption">
+        <span>
+          Confira quantas vezes seus links foram clicados ou acessados através
+          do salto para o link
+        </span>
+      </v-card-subtitle>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="links"
+          disable-pagination
+          hide-default-footer
+          mobile-breakpoint="300"
+          no-data-text="Você ainda não tem nenhum link"
+        >
+          <template v-slot:[`item.label`]="{ item }">
+            <div class="d-flex flex-row align-center">
+              <font-awesome-icon
+                :icon="findIcon(item.media)"
+                style="color: #c4c4c4"
+              />
+              <div class="text-caption ml-2">
+                {{ item.label }}
+              </div>
+            </div>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+  </client-only>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+
+interface Link {
+  id?: number;
+  name?: string;
+  media?: string;
+  url?: string;
+  hide?: boolean;
+  createdAt?: string;
+}
+
+export default Vue.extend({
+  props: {
+    links: { type: Array, default: () => [] },
+  },
+
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Name',
+          align: 'start',
+          sortable: false,
+          value: 'label',
+        },
+        { text: 'Cliques', value: 'clicks', align: 'center' },
+      ],
+    };
+  },
+
+  methods: {
+    ...mapGetters({
+      medias: 'getMedias',
+    }),
+
+    findIcon(media: string) {
+      const medias = this.medias();
+      const found = medias.find((el: Link) => el.media === media);
+      if (found) {
+        return found.icon;
+      }
+      return ['fas', 'question'];
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.link__chip {
+  display: flex;
+  flex-direction: row;
+  // justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  padding: 0.2rem 1rem;
+  border: 1px solid #c4c4c4;
+  border-radius: 5px;
+  width: 100%;
+}
+</style>
