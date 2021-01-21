57a2a15b9c5fe14505b8720276bfb9c2eaf027a7
Diego Rocha <dlmrocha87@gmail.com>
2021-01-21 14:15:00 -0300

    Improved agency profile

--- pages/a/_id.vue
+++ pages/a/_id.vue
@@ -1,33 +1,46 @@
 <template>
-  <v-container fill-height>
-    <v-row class="mx-auto" align="center" justify="center" style="max-width: 900px">
-      <v-col class="text-center">
-        <v-avatar size="150px">
-          <v-img :src="avatar"></v-img>
-        </v-avatar>
-        <div class="text-h6 mt-4">
-          {{ title }}
-        </div>
+  <v-row class="mx-auto fill-height" justify="center" style="max-width: 900px">
+    <v-col class="text-center" align-self="center">
+      <v-avatar size="150px">
+        <v-img :src="avatar"></v-img>
+      </v-avatar>
+      <div class="text-h6 mt-4">{{ title }}</div>
 
-        <v-btn class="mt-4" block depressed color="primary" :to="`/${agency.name}`">contato</v-btn>
+      <v-btn
+        class="mt-4"
+        block
+        depressed
+        tile
+        dark
+        color="#2b2d42"
+        height="50px"
+        :to="`/${agency.name}`"
+      >contato</v-btn>
 
-        <div v-if="agency.bio" class="text-justify mt-6" style="color: #3d405b">
-          {{ bio }}
-        </div>
-      </v-col>
-      <v-col>
-        <v-card class="mx-auto" flat>
-          <v-card-title class="justify-center">
-            {{ header }}
-          </v-card-title>
-          <v-card-text>
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              single-line
-              hide-details
-              ></v-text-field>
+      <div v-if="agency.bio" class="text-justify mt-6" style="color: #3d405b">{{ bio }}</div>
+    </v-col>
+    <v-col>
+      <v-card class="mx-auto" flat>
+        <v-card-title
+          class="justify-center text-h5 font-weight-bold"
+          style="color: #081c15"
+        >{{ header }}</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            rounded
+            filled
+            dense
+            single-line
+            hide-details
+          ></v-text-field>
+          <div
+            v-if="searchLen > 1"
+            class="text-right text-caption my-1 mr-3"
+          >{{ searchLen }} perfis encontrados</div>
+          <div class="list-profiles mt-2">
             <client-only>
               <v-data-table
                 class="row-pointer"
@@ -35,37 +48,37 @@
                 :items="profiles"
                 :search="search"
                 hide-default-header
-                :items-per-page="5"
-                >
-                <template v-slot:no-data>
-                  Ainda não tem nenhum perfil criado!
-                </template>
+                hide-default-footer
+                disable-pagination
+                @pagination="refreshSearchLen"
+              >
+                <template v-slot:no-data>Ainda não tem nenhum perfil criado!</template>
                 <template #item="{ item }">
-                  <v-list-item two-line :href="`https://euyo.me/${item.name}`" >
+                  <v-list-item two-line :href="`https://euyo.me/${item.name}`">
                     <v-list-item-avatar>
                       <v-img :src="getAvatar(item.avatar)"></v-img>
                     </v-list-item-avatar>
                     <v-list-item-content>
-                      <v-list-item-title class="text-caption">
-                          {{ item.title }}
-                      </v-list-item-title>
-                      <v-list-item-subtitle class="font-weight-bold text-caption" style="color: #c4c4c4">
-                          {{ item.name }}
-                      </v-list-item-subtitle>
+                      <v-list-item-title class="text-caption">{{ item.title }}</v-list-item-title>
+                      <v-list-item-subtitle
+                        class="font-weight-bold text-caption"
+                        style="color: #c4c4c4"
+                      >{{ item.name }}</v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
+                  <v-divider inset></v-divider>
                 </template>
               </v-data-table>
             </client-only>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 export default Vue.extend({
   layout: 'profile',
 
@@ -76,7 +89,7 @@ export default Vue.extend({
       );
 
       return {
-        agency
+        agency,
       };
     } catch (error) {
       context.error({
@@ -90,6 +103,7 @@ export default Vue.extend({
     return {
       search: '',
       agency: {},
+      searchLen: 0,
 
       headers: [
         {
@@ -99,7 +113,7 @@ export default Vue.extend({
         },
         { text: 'Nome', value: 'name' },
       ],
-    }
+    };
   },
 
   computed: {
@@ -107,7 +121,7 @@ export default Vue.extend({
       const a: any = this.agency;
       const ps = a.profiles;
       if (ps) {
-        return ps.filter((p: any) => (p.name !== a.name && !p.hiddenAgency));
+        return ps.filter((p: any) => p.name !== a.name && !p.hiddenAgency);
       }
       return [];
     },
@@ -125,7 +139,8 @@ export default Vue.extend({
     },
 
     avatar(): string {
-      const img = (this.agency as any).avatar || 'v1596315038/profile/euyome.jpg';
+      const img =
+        (this.agency as any).avatar || 'v1596315038/profile/euyome.jpg';
       return `https://res.cloudinary.com/euyome/image/upload/${img}`;
     },
   },
@@ -135,8 +150,11 @@ export default Vue.extend({
       const img = avatar || 'v1596315038/profile/euyome.jpg';
       return `https://res.cloudinary.com/euyome/image/upload/${img}`;
     },
-  },
 
+    refreshSearchLen(pagination: any) {
+      this.searchLen = pagination.itemsLength;
+    },
+  },
 
   head(this: any) {
     const title = this.title || this.$route.params.id;
@@ -191,5 +209,29 @@ export default Vue.extend({
       ],
     };
   },
-})
+});
 </script>
+
+<style lang="scss" scoped>
+.list-profiles {
+  max-height: 70vh;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    background-color: #f5f5f5;
+    border-radius: 2px;
+  }
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 2px;
+    background-color: #d9d9d9;
+  }
+}
+</style>
