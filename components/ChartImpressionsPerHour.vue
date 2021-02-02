6528fb5a6ec454a326fef6dd0ba3faaac6f12a43
Diego Rocha <dlmrocha87@gmail.com>
2021-02-02 15:48:39 -0300

    Introduces impressions per hour

---
+++ components/ChartImpressionsPerHour.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <canvas
+      id="chart-impressions-per-hour"
+      width="100%"
+      max-height="250px"
+      style="min-height: 200px"
+    ></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable no-unused-vars */
+import Vue from 'vue';
+import { Chart } from 'chart.js';
+
+export default Vue.extend({
+  props: {
+    data: { type: Object, required: true },
+  },
+
+  mounted() {
+    const canvas = document.getElementById(
+      'chart-impressions-per-hour'
+    ) as HTMLCanvasElement;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chart = new Chart(canvas, {
+      type: 'bar',
+      data: this.data,
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        spanGaps: false,
+        plugins: {
+          filler: {
+            propagate: false,
+          },
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          callbacks: {
+            label(tooltipItem: any) {
+              return tooltipItem.yLabel;
+            },
+          },
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: '#e8e8e4',
+              },
+              ticks: {
+                beginAtZero: true,
+                stepSize: 2,
+              },
+            },
+          ],
+        },
+      },
+    });
+  },
+});
+</script>
--- pages/s/_id.vue
+++ pages/s/_id.vue
@@ -2,61 +2,58 @@
   <v-container fill-height>
     <v-row>
       <v-col class="text-center">
-        <v-avatar size="150px">
-          <v-img :src="avatar"></v-img>
-        </v-avatar>
-        <div class="text-h6 mt-4">
-          {{ profile.title }}
-        </div>
+        <ProfileHeader :profile="profile"></ProfileHeader>
         <v-btn text small color="primary" :to="`/${profile.name}`">Ir para o perfil</v-btn>
+
         <v-card class="mt-4">
-          <v-card-title>
-            Visualizações Diárias
-          </v-card-title>
+          <v-card-title>Impressões Diárias</v-card-title>
           <v-card-text>
             <client-only>
-              <DailyViewsChart
-                v-if="chartData.datasets"
-                :data="chartData"
-                style="width: 100%"
-                />
+              <DailyViewsChart v-if="chartData.datasets" :data="chartData" style="width: 100%" />
             </client-only>
           </v-card-text>
         </v-card>
+
+        <v-card class="mt-4">
+          <v-card-title>Impressões por hora (Hoje)</v-card-title>
+          <v-card-text>
+            <client-only>
+              <ChartImpressionsPerHour
+                v-if="chartImpressionsPerHourData.datasets"
+                :data="chartImpressionsPerHourData"
+                style="width: 100%"
+              />
+            </client-only>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card width="100%" class="mt-2">
+          <v-card-title>Impressões</v-card-title>
+          <v-card-subtitle class="text-caption">Quantas vezes seu perfil foi acessado no total.</v-card-subtitle>
+          <v-card-text>
+            <div class="text-center pa-1">
+              <span class="text__style">{{ stats.impressions || 0 }}</span>
+            </div>
+          </v-card-text>
+        </v-card>
+
         <v-card v-if="stats.card" width="100%" class="mt-2">
           <v-card-title>Estatísticas do Card</v-card-title>
           <v-card-text>
             <v-row align="center" no-gutters>
               <v-col class="text-center">
                 <div class="text-caption font-weight-bold">Visualizações</div>
-                <span class="text-h5">
-                  {{ stats.card.impressions || 0 }}
-                </span>
+                <span class="text-h5">{{ stats.card.impressions || 0 }}</span>
               </v-col>
               <v-col class="text-center">
                 <div class="text-caption font-weight-bold">Cliques</div>
-                <span class="text-h5">
-                  {{ stats.card.clicks || 0 }}
-                </span>
+                <span class="text-h5">{{ stats.card.clicks || 0 }}</span>
               </v-col>
             </v-row>
           </v-card-text>
         </v-card>
-      </v-col>
-      <v-col>
-        <v-card width="100%" class="mt-2">
-          <v-card-title> Impressões </v-card-title>
-          <v-card-subtitle class="text-caption">
-            Quantas vezes seu perfil foi acessado no total.
-          </v-card-subtitle>
-          <v-card-text>
-            <div class="text-center pa-1">
-              <span class="text__style">
-                {{ stats.impressions || 0 }}
-              </span>
-            </div>
-          </v-card-text>
-        </v-card>
+
         <LinksStats :links="stats.links" class="mt-2 mb-8" />
       </v-col>
     </v-row>
@@ -92,12 +89,45 @@ export default Vue.extend({
     return {
       stats: {},
       chartData: {},
+      chartImpressionsPerHourData: {},
       profile: {},
     };
   },
 
   mounted() {
-    const { dailyViews } = this.stats as any;
+    const { dailyViews, impressionsLast24Hours } = this.stats as any;
+
+    if (impressionsLast24Hours?.length > 0) {
+      const now = new Date();
+      const timezoneOffsetInHours = now.getTimezoneOffset() / 60;
+
+      // Generate an 24 hours array
+      const adata = [...Array(24).keys()];
+      const data = adata.map((x: number) => {
+        const y = impressionsLast24Hours.find((val: any) => {
+          const date = new Date(val._id);
+          const hours = date.getUTCHours() - timezoneOffsetInHours;
+
+          return hours === x;
+        });
+        const impressions = y?.impressions || 0;
+        return { hour: `${x}h`, impressions };
+      });
+
+      this.chartImpressionsPerHourData = {
+        labels: data.map((stat: any) => {
+          return stat.hour.toString();
+        }),
+
+        datasets: [
+          {
+            backgroundColor: '#21c25e',
+            data: data.map((stat: any) => stat.impressions),
+          },
+        ],
+      };
+    }
+
     if (dailyViews && dailyViews.length > 0) {
       this.chartData = {
         labels: dailyViews.map((stat: any) => {
@@ -110,10 +140,10 @@ export default Vue.extend({
         datasets: [
           {
             backgroundColor: '#41B38A33',
-            borderColor: '#41B38A',
+            borderColor: '#21c25e',
             data: dailyViews.map((stat: any) => stat.views),
             fill: true,
-            pointRadius: 7,
+            pointRadius: 5,
             pointHoverRadius: 10,
           },
         ],
@@ -123,7 +153,8 @@ export default Vue.extend({
 
   computed: {
     avatar(): string {
-      const img = (this.profile as any).avatar || 'v1596315038/profile/euyome.jpg';
+      const img =
+        (this.profile as any).avatar || 'v1596315038/profile/euyome.jpg';
       return `https://res.cloudinary.com/euyome/image/upload/${img}`;
     },
     code(): string {
@@ -134,7 +165,7 @@ export default Vue.extend({
   methods: {
     async genStatsCode(): Promise<void> {
       await this.$store.dispatch('profile/genStatsCode');
-    }
+    },
   },
 
   head(this: any) {
